6e9b8cd22d262677ad69b085ce5fa59c56eab048
fahmida-urmi <fahmidarahmn@gmail.com>
2025-04-08 11:43:11 +0600

    completed my resume

---
+++ script.js
@@ -0,0 +1,28 @@
+
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tabLink of tabLinks){
+        tabLink.classList.remove("active-link");
+    }
+
+    for(tabContent of tabContents){
+        tabContent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+var sidemenu = document.getElementById("sidemenu");
+
+
+function openmenu(){
+    sidemenu.style.right = "0"
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px"
+}
